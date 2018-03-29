88c0994984ad72160cce57e2fd0a2738b7339e39
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-03-29 16:29:43 -0500

    added validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  votos       :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Question < ApplicationRecord
-  validates :title, :description, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+
 end
