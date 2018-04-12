1732862bb1627083ca1b46e68fb92ad312056976
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-04-12 17:01:25 -0500

    fixed some errors and perfected some forms

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,6 +15,7 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
 
+  validates :body, presence: true
   validates :commentable_type, :commentable_id, :user_id, :body, presence: :true
 
 end
